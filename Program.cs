e91c26863c68deb2ba5c411ad2058f0d6ea3004e
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2023-02-08 14:26:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorial
+
+{
+    internal class Program
+    {
+        static int iteractiveFactorial(int N)
+        { // Iterative Function Using Loop
+            int Fact = 1;
+            while (N > 1)
+            {
+                Console.Write("{0} * ", N);
+                //cout << N << " * ";
+                Fact *= N--;
+            }
+            Console.Write("{0} ", N);
+            //cout << N;
+            return Fact;
+        }
+
+        static int recursiveFactorial(int N)
+        { // Recursive Function
+            return 0;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int N;
+            do
+            {
+                Console.Write("Enter a positive integer (maximum value 10): ");
+                N = int.Parse(Console.ReadLine());
+            } while (N <= 0 || N > 15);
+            Console.WriteLine("\n");
+            Console.WriteLine("\nIterative Factorial {0} = " + iteractiveFactorial(N).ToString());
+            Console.WriteLine("\n");
+            Console.WriteLine("\nRecursive Factorial {0} = " + recursiveFactorial(N).ToString());
+            Console.ReadKey();
+        }
+    }
+}
